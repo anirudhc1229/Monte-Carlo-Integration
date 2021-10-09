d67ed70d31e0f421bfe0bd332a34205e3621f0aa
Anirudh Chari <73615691+anirudhc1229@users.noreply.github.com>
2021-10-09 17:03:33 -0500

    Add files via upload

---
+++ monteCarloIntegrationEx.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import os
+import time
+
+# GOAL: Approximate the integral of function f from lower bound a to upper bound b using Monte Carlo simulation
+
+a = 0       # lower bound of integration
+b = np.pi   # upper bound of integration
+f = np.sin  # function to integrate
+N = 10000   # sample size
+
+def mcIntegrate(p):
+    xrand = np.random.uniform(a, b, N)       # create array filled with random numbers within bounds
+    integral = np.sum(f(xrand))                 # sum return values of function of each random number
+    approx = integral * ((b - a) / float(N))    # scale integral by difference of bounds divided by sample size
+    return approx
+
+
+if __name__ == "__main__":
+    
+    start = time.time()
+    # run simulation N times in parallel and store results in array
+    with Pool(os.cpu_count() + 1) as pool:
+        areas = pool.map(mcIntegrate, range(N))
+    end = time.time()
+    print(end - start)
+
+    # graph approximation distribution
+    plt.title("Distribution of Approximated Integrals")
+    plt.hist(areas, bins=30, ec='black')
+    plt.xlabel("Areas")
+    plt.show()
